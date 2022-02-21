3ddfc176250319e045bce7898fa6d10132e3f50e
Simon Parten <quafadas@gmail.com>
2022-02-21 15:53:02 +0100

    Now test

--- src/test/scala/VizTest.scala
+++ src/test/scala/VizTest.scala
@@ -14,10 +14,9 @@
  * limitations under the License.
  */
 
-class MySuite extends munit.FunSuite {
+class MySuite extends munit.FunSuite:
   test("hello") {
     val obtained = 43
     val expected = 43
     assertEquals(obtained, expected)
   }
-}
