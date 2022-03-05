73d4970492489f60b020a4255d329ba8e04bfe35
Simon Parten <quafadas@gmail.com>
2022-03-05 16:08:09 +0100

    Sort doc and extension method issues

--- jvm/src/test/scala/Extensions.scala
+++ jvm/src/test/scala/Extensions.scala
@@ -14,14 +14,15 @@
  * limitations under the License.
  */
 
-import viz.extensions.*
+import viz.extensions.jvm.*
 import viz.PlotTargets.websocket // for local testing
 //import viz.PlotTargets.doNothing // for CI... as we don't have a port available...
 import scala.util.Random
 
-class ExtensionSuite extends munit.FunSuite:
+class ExtensionSuiteJVM extends munit.FunSuite:
   test("extension methods exist... ") {
 
+    val randomTuple_Int_Double: IndexedSeq[(Int, Double)] = (0 to 20).map(i => (i, i * Random.nextDouble()))
     randomTuple_Int_Double.plotRegression()
 
   }
