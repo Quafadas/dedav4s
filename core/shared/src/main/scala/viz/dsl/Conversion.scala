133251a89cd78bdeffd1d840286d533f28f4eaec
Simon Parten <quafadas@gmail.com>
2022-05-11 16:32:36 +0200

    Helpers for plotting

--- core/shared/src/main/scala/viz/dsl/Conversion.scala
+++ core/shared/src/main/scala/viz/dsl/Conversion.scala
@@ -1,11 +1,33 @@
 package viz.dsl
 
 import scala.language.implicitConversions
+import viz.PlotTarget
 import io.circe.syntax.*
 import io.circe.Encoder
+import viz.dsl.vega.VegaDsl
+import viz.PlatformShow
+import viz.dsl.vegaLite.VegaLiteDsl
 
-object Conversion :
-    extension [T](moreJson: T)(using enc: Encoder[T])
-        def u: ujson.Value =
-            val enc = summon[Encoder[T]]
-            ujson.read(enc(moreJson).toString)
+object Conversion:
+  extension [T](moreJson: T)(using enc: Encoder[T])
+    def u: ujson.Value =
+      val enc = summon[Encoder[T]]
+      ujson.read(enc(moreJson).toString)
+
+case class DslSpec(in: VegaDsl | VegaLiteDsl)(using PlotTarget) extends PlatformShow:
+  override def spec =
+    in match
+      case v: VegaDsl      => v.asJson.deepDropNullValues.toString
+      case vl: VegaLiteDsl => vl.asJson.deepDropNullValues.toString
+
+object DslPlot:
+  extension (vega: VegaDsl)
+    def plot(using PlotTarget) =
+      DslSpec(vega)
+  extension (vega: VegaLiteDsl)
+    def plot(using PlotTarget) =
+      DslSpec(vega)
+
+  extension (vega: VegaDsl | VegaLiteDsl)
+    def plot(using PlotTarget) =
+      DslSpec(vega)
