aec8fd116d8725e423cd9e4ad15ac2009afd861d
Simon Parten <quafadas@gmail.com>
2022-05-12 14:08:15 +0200

    aaaahhh

--- jsdocs/src/main/Main.scala
+++ jsdocs/src/main/Main.scala
@@ -2,15 +2,9 @@ package jsdocs
 
 object Main {
   def main(args: Array[String]): Unit = {      
-    val anId = inDiv.id
-    val newId = if anId.isEmpty then
-      val temp = java.util.UUID.randomUUID()
-      inDiv.setAttribute("id", temp.toString())
-    else anId
-
     val opts = viz.vega.facades.EmbedOptions
     val parsed = JSON.parse(spec)
-    viz.vega.facades.VegaEmbed.embed(s"#$anId", parsed, opts)
+    viz.vega.facades.VegaEmbed.embed(s"#", parsed, opts)
     ()/*  */
   }
 }
