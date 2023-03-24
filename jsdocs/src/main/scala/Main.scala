a2a7a1362bd9ef5b31943fdb66a459b81614cfdd
Simon Parten <Quafadas@users.noreply.github.com>
2023-03-24 22:31:33 +0100

    Try require JS

--- jsdocs/src/main/scala/Main.scala
+++ jsdocs/src/main/scala/Main.scala
@@ -2,12 +2,24 @@ package jsdocs
 
 import scala.scalajs.js.JSON
 import viz.vega.facades.EmbedOptions
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+
 
 object Main {
-  def main(args: Array[String]): Unit = {      
+  def main(args: Array[String]): Unit = {    
+    println("called here")  
     val opts = viz.vega.facades.EmbedOptions
     val parsed = JSON.parse("")
     viz.vega.facades.VegaEmbed.embed(s"#", parsed, opts)
+    require("vega-embed", x => js.Object )
     ()/*  */
   }
+}
+
+
+@js.native
+@JSImport("require", JSImport.Namespace)
+object require extends js.Object {  
+  def apply(n: String, f : Function1[js.Any, js.Any]): String = js.native
 }
