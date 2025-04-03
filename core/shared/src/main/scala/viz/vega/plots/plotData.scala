d3adc6d4bdea8f80f780032355b04fe339a944b1
Simon Parten <Quafadas@users.noreply.github.com>
2025-04-03 14:53:28 +0000

    .

--- core/shared/src/main/scala/viz/vega/plots/plotData.scala
+++ core/shared/src/main/scala/viz/vega/plots/plotData.scala
@@ -16,5 +16,5 @@
 
 package viz.vega.plots
 
-type BarPlotDataEntry = (amount: Double,  category: String)
-type PiePlotDataEntry = (  field: Double,  id: String )
+type BarPlotDataEntry = (amount: Double, category: String)
+type PiePlotDataEntry = (field: Double, id: String)
