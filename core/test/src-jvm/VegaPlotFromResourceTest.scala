de99bf49c363f564e6f78dc3f2bd2f26de797904
Simon Parten <quafadas@gmail.com>
2026-01-26 08:59:37 +0100

    .

--- core/test/src-jvm/VegaPlotFromResourceTest.scala
+++ core/test/src-jvm/VegaPlotFromResourceTest.scala
@@ -2,15 +2,20 @@ package viz.macros
 
 import munit.FunSuite
 import io.circe.*
+import io.circe.syntax.*
 import io.circe.optics.JsonPath.*
+import viz.NtCirce.given
 
 class VegaPlotFromResourceTest extends FunSuite:
 
   test("array access with fromResource - tuple-style accessor for heterogeneous arrays") {
     val spec = VegaPlot.fromResource("arr.vl.json")
     val result = spec.build(
+      _.layer.head.data.values := List((x = 1)).asJson ,
       _.layer._1.data.sequence.start := 2
     )
+
+
 
     // Verify the change was applied
     val updatedStart = result.hcursor
